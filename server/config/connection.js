3ff76daa0853c779cfede9854bb51223245bb3c5
dcast0607 <100454484+dcast0607@users.noreply.github.com>
2022-09-10 12:44:26 -0700

    More changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoAtlasUri = encodeURI(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/no-name');
+const mongoAtlasUri = encodeURI(process.env.MONGODB_URI) || 'mongodb://127.0.0.1:27017/no-name';
 
 mongoose.connect(mongoAtlasUri, {
   useNewUrlParser: true,
